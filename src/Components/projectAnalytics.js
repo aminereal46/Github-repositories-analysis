67b3416703ae9ae35be098f97d7ca1bece9a1d10
aminereal46 <amine.hajji.1993@gmail.com>
2018-06-14 02:03:19 +0200

    'chartAdded'

--- src/Components/projectAnalytics.js
+++ src/Components/projectAnalytics.js
@@ -66,35 +66,31 @@ class Project extends Component {
         } while (!all)
        }
        console.log('commits:'+commits.length);
+       this.setState({
+         loading:''
+       });
        commits.sort((a,b)=>
           new Date(a.commit.date) - new Date(b.commit.date)
         );
-        this.setState({
-          loading : '',
-          menu : <div>
-          <button >
-                Committers
-              </button>
-              <button >
-                Commits
-              </button>
-              </div>
-        });
-
        console.log('count####',json.length,' ',json);
        let a = 0;
-
+       let coms = [];
+       let committers = {};
        commits.map(commit =>{
          a++;
-         let user = commit.commit.committer.name;
-         if(!this.state.committersMap[user]){
-             this.state.committersMap[user]= 0;
-         }
-         console.log('ccc');
-           this.state.committersMap[user] = this.state.committersMap[user]+1;
+         let com = commit.commit.committer.name;
+
+         committers[com]=1;
        });
        commits.slice(0,100).map(commit =>{
          let user = commit.commit.committer.name;
+         let com = {
+              message : commit.commit.message,
+              commiter : user,
+              date : commit.commit.committer.date
+            };
+
+         coms.push(com);
          if(!this.state.committersWithLastCommits[user]){
 
              this.state.committersWithLastCommits[user] = 0;
@@ -104,12 +100,19 @@ class Project extends Component {
        });
 
        this.setState({
-         allCommitters : Object.keys(this.state.committersMap).map(key => {
-           let commitsNbr = this.state.committersMap[key];
+         allCommitters : Object.keys(committers).map(committer => {
            return (
-           <div key={key}>
-             {key}({commitsNbr})
+           <div key={committer}>
+             {committer}
            </div>);
+         }),
+         commits : coms.map(com => {
+           return (
+           <div key={com.commiter+com.date}>
+             <div>{com.message}</div>
+             {com.commiter}   commited on {com.date.split('T')[0]} <br/><br/>
+           </div>);
+
          }),
          committers : Object.keys(this.state.committersWithLastCommits).map(key => {
            let commitsNbr = this.state.committersWithLastCommits[key];
@@ -121,14 +124,29 @@ class Project extends Component {
      });
    });
   }
+
+  showCommitters(){
+
+
+  }
+
   render() {
     return (
     <div>
       {this.state.menu}
       {this.state.loading}
-      {this.state.allCommitters}
-      ###########################################
-      {this.state.committers}
+      <div className='commits'>
+        {this.state.allCommitters}
+      </div>
+
+        <div className='commits'>
+          {this.state.committers}
+        </div>
+        ###########################################
+
+        <div className='commits'>
+          {this.state.commits}
+        </div>
     </div>
 
     );
